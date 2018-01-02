798918cd34c730cb9afbfb90fc0d84ac375b5501
David DiBenedetto <dibend8@gmail.com>
2018-01-02 00:47:11 -0500

    init

---
+++ resources/js/Message.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+export default class Message extends Component {
+
+  getAvatar(messageType){
+    if(this.props.type == messageType) {
+      
+      return (
+        <div className="col-md-2 col-xs-2 avatar">
+           {this.props.author}
+        </div>
+      );
+    }
+  }
+
+  render() {
+
+    return (
+        <div className={"row msg_container base_" + this.props.type}>
+          {this.getAvatar('receive')}
+          <div className="col-md-10 col-xs-10">
+            <div className={"messages msg_" + this.props.type}>
+               <p>{this.props.children}</p>
+            </div>
+          </div>
+          {this.getAvatar('sent')}
+       </div>
+    );
+
+  }
+}
